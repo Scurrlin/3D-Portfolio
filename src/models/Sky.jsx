3f71ad03206d2ee535c38a0627ae6146380e163c
Sean Currlin <sean.currlin@gmail.com>
2024-01-16 10:27:53 -0500

    update bird and plane models

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
-const Sky = () => {
+import skyScene from "../assets/3d/sky.glb";
+
+// 3D Model from: https://sketchfab.com/3d-models/phoenix-bird-844ba0cf144a413ea92c779f18912042
+export function Sky({ isRotating }) {
+  const sky = useGLTF(skyScene);
+  const skyRef = useRef();
+
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.25 * delta;
+  });
+
   return (
-    <mesh></mesh>
-  )
+    <mesh ref={skyRef}>
+      <primitive object={sky.scene} />
+    </mesh>
+  );
 }
-
-export default Sky
