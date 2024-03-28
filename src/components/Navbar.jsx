4de3939fef50cab443cd6a26c1c9f9ee5bce3d45
Sean Currlin <sean.currlin@gmail.com>
2024-03-28 14:32:56 -0400

    add resume link to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,9 @@ const Navbar = () => {
         <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
+        <a href="https://drive.google.com/file/d/1bIeq3KNMCQ9EL5IgcCIQ96Jc7JpCdx81/view?usp=sharing" target='_blank' rel='noopener noreferrer' className="text-black">
+          Resume
+        </a>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
           About
         </NavLink>
