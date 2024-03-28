247f1a24012c8f8ceeec93e9e28d14ad55f3fb4d
Sean Currlin <sean.currlin@gmail.com>
2024-03-28 15:52:29 -0400

    add icon names in About section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -37,6 +37,7 @@ import {
                   <img
                     src={skill.imageUrl}
                     alt={skill.name}
+                    title={skill.name}
                     className='w-1/2 h-1/2 object-contain'
                   />
                 </div>
