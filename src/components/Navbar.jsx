ba683ba282c124b068c9a019123ce9e9b521abd7
Sean Currlin <sean.currlin@gmail.com>
2024-09-07 09:29:12 -0400

    update resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
       </NavLink>
       <nav className='flex text-base gap-3 font-medium'>
-        <a href="https://drive.google.com/file/d/1ln2ejh70Y-VIG-QgD-LUxux4lwkGMAzY/view?usp=sharing" target='_blank' rel='noopener noreferrer' className="text-black">
+        <a href="https://drive.google.com/file/d/1SuZSfocMiZgQ2z4FAMi0EZie6sRZkEKr/view?usp=sharing" target='_blank' rel='noopener noreferrer' className="text-black">
           Resume
         </a>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
